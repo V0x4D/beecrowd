72292b3dabda593157c9917ef1561e9b730ee3d1
Vinícius M. <149252600+V0x4D@users.noreply.github.com>
2024-08-11 17:25:05 -0300

    Create 1001.c

---
+++ 1001.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int A = 0;
+    int B = 0;
+    int X = 0;
+    
+    scanf("%i", &A);
+    
+    scanf("%i", &B);
+    
+    X = A + B;
+    
+    printf("X = %i\n", X);
+    
+    
+    return 0;
+  
+}
