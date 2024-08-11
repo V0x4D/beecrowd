94a2ce98a99105d8651d78b6bd03caf4a8d263ef
Vinícius M. <149252600+V0x4D@users.noreply.github.com>
2024-08-11 17:46:58 -0300

    Create 1016.c

---
+++ 1016.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	int Distancia;
+	
+	scanf("%i", &Distancia);
+	
+	Distancia = Distancia * 2;
+	
+	printf("%i minutos\n", Distancia);
+	
+	return 0;
+}
